e7fe5051e60190466e68eded2187a92e9438fa79
bhj12 <57935761+bhj12@users.noreply.github.com>
2021-02-26 18:15:49 +0500

    Initial commit  my first upload by bhj

---
+++ app/src/main/java/com/bhj/kbg/testprojectforinapppurchase/MainActivity.java
@@ -0,0 +1,366 @@
+package com.bhj.kbg.testprojectforinapppurchase;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.billingclient.api.AcknowledgePurchaseParams;
+import com.android.billingclient.api.AcknowledgePurchaseResponseListener;
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.List;
+import static com.android.billingclient.api.BillingClient.SkuType.SUBS;
+
+public class MainActivity extends AppCompatActivity implements PurchasesUpdatedListener {
+
+    public static final String PREF_FILE= "MyPref";
+    public static final String SUBSCRIBE_KEY= "subscribe";
+    public static final String ITEM_SKU_SUBSCRIBE= "sub_example";
+
+    TextView premiumContent,subscriptionStatus;
+    Button subscribe;
+
+    private BillingClient billingClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        premiumContent = (TextView) findViewById(R.id.premium_content);
+        subscriptionStatus=(TextView) findViewById(R.id.subscription_status);
+        subscribe=(Button) findViewById(R.id.subscribe);
+
+        // Establish connection to billing client
+        //check subscription status from google play store cache
+        //to check if item is already Subscribed or subscription is not renewed and cancelled
+        billingClient = BillingClient.newBuilder(this).enablePendingPurchases().setListener(this).build();
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(BillingResult billingResult) {
+                if(billingResult.getResponseCode()==BillingClient.BillingResponseCode.OK){
+                    Purchase.PurchasesResult queryPurchase = billingClient.queryPurchases(SUBS);
+                    List<Purchase> queryPurchases = queryPurchase.getPurchasesList();
+                    if(queryPurchases!=null && queryPurchases.size()>0){
+                        handlePurchases(queryPurchases);
+                    }
+                    //if no item in purchase list means subscription is not subscribed
+                    //Or subscription is cancelled and not renewed for next month
+                    // so update pref in both cases
+                    // so next time on app launch our premium content will be locked again
+                    else{
+                        saveSubscribeValueToPref(false);
+                    }
+                }
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                Toast.makeText(getApplicationContext(),"Service Disconnected",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //item subscribed
+        if(getSubscribeValueFromPref()){
+            subscribe.setVisibility(View.GONE);
+            premiumContent.setVisibility(View.VISIBLE);
+            subscriptionStatus.setText("Subscription Status : Subscribed");
+        }
+        //item not subscribed
+        else{
+            premiumContent.setVisibility(View.GONE);
+            subscribe.setVisibility(View.VISIBLE);
+            subscriptionStatus.setText("Subscription Status : Not Subscribed");
+        }
+    }
+
+    private SharedPreferences getPreferenceObject() {
+        return getApplicationContext().getSharedPreferences(PREF_FILE, 0);
+    }
+    private SharedPreferences.Editor getPreferenceEditObject() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(PREF_FILE, 0);
+        return pref.edit();
+    }
+    private boolean getSubscribeValueFromPref(){
+        return getPreferenceObject().getBoolean( SUBSCRIBE_KEY,false);
+    }
+    private void saveSubscribeValueToPref(boolean value){
+        getPreferenceEditObject().putBoolean(SUBSCRIBE_KEY,value).commit();
+    }
+
+    //initiate purchase on button click
+    public void subscribe(View view) {
+        //check if service is already connected
+        if (billingClient.isReady()) {
+            initiatePurchase();
+        }
+        //else reconnect service
+        else{
+            billingClient = BillingClient.newBuilder(this).enablePendingPurchases().setListener(this).build();
+            billingClient.startConnection(new BillingClientStateListener() {
+                @Override
+                public void onBillingSetupFinished(BillingResult billingResult) {
+                    if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                        initiatePurchase();
+                    } else {
+                        Toast.makeText(getApplicationContext(),"Error "+billingResult.getDebugMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+                @Override
+                public void onBillingServiceDisconnected() {
+                    Toast.makeText(getApplicationContext(),"Service Disconnected ",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+    private void initiatePurchase() {
+        List<String> skuList = new ArrayList<>();
+        skuList.add(ITEM_SKU_SUBSCRIBE);
+        SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+        params.setSkusList(skuList).setType(SUBS);
+        BillingResult billingResult = billingClient.isFeatureSupported(BillingClient.FeatureType.SUBSCRIPTIONS);
+        if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+            billingClient.querySkuDetailsAsync(params.build(),
+                    new SkuDetailsResponseListener() {
+                        @Override
+                        public void onSkuDetailsResponse(BillingResult billingResult,
+                                                         List<SkuDetails> skuDetailsList) {
+                            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                                if (skuDetailsList != null && skuDetailsList.size() > 0) {
+                                    BillingFlowParams flowParams = BillingFlowParams.newBuilder()
+                                            .setSkuDetails(skuDetailsList.get(0))
+                                            .build();
+                                    billingClient.launchBillingFlow(MainActivity.this, flowParams);
+                                } else {
+                                    //try to add subscription item "sub_example" in google play console
+                                    Toast.makeText(getApplicationContext(), "Item not Found", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        " Error " + billingResult.getDebugMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    "Sorry Subscription not Supported. Please Update Play Store", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onPurchasesUpdated(BillingResult billingResult, @Nullable List<Purchase> purchases) {
+        //if item subscribed
+        if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && purchases != null) {
+            handlePurchases(purchases);
+        }
+        //if item already subscribed then check and reflect changes
+        else if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED) {
+            Purchase.PurchasesResult queryAlreadyPurchasesResult = billingClient.queryPurchases(SUBS);
+            List<Purchase> alreadyPurchases = queryAlreadyPurchasesResult.getPurchasesList();
+            if(alreadyPurchases!=null){
+                handlePurchases(alreadyPurchases);
+            }
+        }
+        //if Purchase canceled
+        else if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.USER_CANCELED) {
+            Toast.makeText(getApplicationContext(),"Purchase Canceled",Toast.LENGTH_SHORT).show();
+        }
+        // Handle any other error msgs
+        else {
+            Toast.makeText(getApplicationContext(),"Error "+billingResult.getDebugMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+    void handlePurchases(List<Purchase>  purchases) {
+        for(Purchase purchase:purchases) {
+            //if item is purchased
+            if (ITEM_SKU_SUBSCRIBE.equals(purchase.getSku()) && purchase.getPurchaseState() == Purchase.PurchaseState.PURCHASED)
+            {
+                if (!verifyValidSignature(purchase.getOriginalJson(), purchase.getSignature())) {
+                    // Invalid purchase
+                    // show error to user
+                    Toast.makeText(getApplicationContext(), "Error : invalid Purchase", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // else purchase is valid
+                //if item is purchased and not acknowledged
+                if (!purchase.isAcknowledged()) {
+                    AcknowledgePurchaseParams acknowledgePurchaseParams =
+                            AcknowledgePurchaseParams.newBuilder()
+                                    .setPurchaseToken(purchase.getPurchaseToken())
+                                    .build();
+                    billingClient.acknowledgePurchase(acknowledgePurchaseParams, ackPurchase);
+                }
+                //else item is purchased and also acknowledged
+                else {
+                    // Grant entitlement to the user on item purchase
+                    // restart activity
+                    if(!getSubscribeValueFromPref()){
+                        saveSubscribeValueToPref(true);
+                        Toast.makeText(getApplicationContext(), "Item Purchased", Toast.LENGTH_SHORT).show();
+                        this.recreate();
+                    }
+                }
+            }
+            //if purchase is pending
+            else if( ITEM_SKU_SUBSCRIBE.equals(purchase.getSku()) && purchase.getPurchaseState() == Purchase.PurchaseState.PENDING)
+            {
+                Toast.makeText(getApplicationContext(),
+                        "Purchase is Pending. Please complete Transaction", Toast.LENGTH_SHORT).show();
+            }
+            //if purchase is unknown mark false
+            else if(ITEM_SKU_SUBSCRIBE.equals(purchase.getSku()) && purchase.getPurchaseState() == Purchase.PurchaseState.UNSPECIFIED_STATE)
+            {
+                saveSubscribeValueToPref(false);
+                premiumContent.setVisibility(View.GONE);
+                subscribe.setVisibility(View.VISIBLE);
+                subscriptionStatus.setText("Subscription Status : Not Subscribed");
+                Toast.makeText(getApplicationContext(), "Purchase Status Unknown", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    AcknowledgePurchaseResponseListener ackPurchase = new AcknowledgePurchaseResponseListener() {
+        @Override
+        public void onAcknowledgePurchaseResponse(BillingResult billingResult) {
+            if(billingResult.getResponseCode()==BillingClient.BillingResponseCode.OK){
+                //if purchase is acknowledged
+                // Grant entitlement to the user. and restart activity
+                saveSubscribeValueToPref(true);
+                MainActivity.this.recreate();
+            }
+        }
+    };
+
+    /**
+     * Verifies that the purchase was signed correctly for this developer's public key.
+     * <p>Note: It's strongly recommended to perform such check on your backend since hackers can
+     * replace this method with "constant true" if they decompile/rebuild your app.
+     * </p>
+     */
+    private boolean verifyValidSignature(String signedData, String signature) {
+        try {
+            // To get key go to Developer Console > Select your app > Development Tools > Services & APIs.
+            String base64Key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoizuZjPAIC+Dwsz/CocNXi7v2cVk3o9Arw383RUxP816e/cfTkrurEfubIl18ZuUo2PDVAPnAxXrn7BHnUJvpU8AD8601Rwg/dbT2WyQgBhkHStGiniKjl3k/668a69YFXohlbBwqJpp1EUPwyfE5d3OBb3QlwSY5mxjvxNicqr+ZcS3HscaYYEzlwjDFVhuNBk4zdN131Ttb26WWNKrLu3rKKaqhBQddREKQpqEL/aKGao3Z3AHAszcfzMJQR1Z2SoCl03quAdAO/ZtuW52hwSOSzvunmPIcnex/LHZ20NgHAXDGi+6cRT0TnrPZBykFE3PgnuHJdiREmbGD7XcxwIDAQAB";     // enter key here
+            return Security.verifyPurchase(base64Key, signedData, signature);
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(billingClient!=null){
+            billingClient.endConnection();
+        }
+    }
+}
+// start security class
+class Security {
+    private static final String TAG = "IABUtil/Security";
+
+    private static final String KEY_FACTORY_ALGORITHM = "RSA";
+    private static final String SIGNATURE_ALGORITHM = "SHA1withRSA";
+
+    /**
+     * Verifies that the data was signed with the given signature, and returns the verified
+     * purchase.
+     *
+     * @param base64PublicKey the base64-encoded public key to use for verifying.
+     * @param signedData      the signed JSON string (signed, not encrypted)
+     * @param signature       the signature for the data, signed with the private key
+     * @throws IOException if encoding algorithm is not supported or key specification
+     *                     is invalid
+     */
+    public static boolean verifyPurchase(String base64PublicKey, String signedData,
+                                         String signature) throws IOException {
+        if (TextUtils.isEmpty(signedData) || TextUtils.isEmpty(base64PublicKey)
+                || TextUtils.isEmpty(signature)) {
+            //Purchase verification failed: missing data
+            return false;
+        }
+
+        PublicKey key = generatePublicKey(base64PublicKey);
+        return verify(key, signedData, signature);
+    }
+
+    /**
+     * Generates a PublicKey instance from a string containing the Base64-encoded public key.
+     *
+     * @param encodedPublicKey Base64-encoded public key
+     * @throws IOException if encoding algorithm is not supported or key specification
+     *                     is invalid
+     */
+    public static PublicKey generatePublicKey(String encodedPublicKey) throws IOException {
+        try {
+            byte[] decodedKey = Base64.decode(encodedPublicKey, Base64.DEFAULT);
+            KeyFactory keyFactory = KeyFactory.getInstance(KEY_FACTORY_ALGORITHM);
+            return keyFactory.generatePublic(new X509EncodedKeySpec(decodedKey));
+        } catch (NoSuchAlgorithmException e) {
+            // "RSA" is guaranteed to be available.
+            throw new RuntimeException(e);
+        } catch (InvalidKeySpecException e) {
+            String msg = "Invalid key specification: " + e;
+            throw new IOException(msg);
+        }
+    }
+
+    /**
+     * Verifies that the signature from the server matches the computed signature on the data.
+     * Returns true if the data is correctly signed.
+     *
+     * @param publicKey  public key associated with the developer account
+     * @param signedData signed data from server
+     * @param signature  server signature
+     * @return true if the data and signature match
+     */
+    public static boolean verify(PublicKey publicKey, String signedData, String signature) {
+        byte[] signatureBytes;
+        try {
+            signatureBytes = Base64.decode(signature, Base64.DEFAULT);
+        } catch (IllegalArgumentException e) {
+            //Base64 decoding failed
+            return false;
+        }
+        try {
+            Signature signatureAlgorithm = Signature.getInstance(SIGNATURE_ALGORITHM);
+            signatureAlgorithm.initVerify(publicKey);
+            signatureAlgorithm.update(signedData.getBytes());
+            if (!signatureAlgorithm.verify(signatureBytes)) {
+                //Signature verification failed
+                return false;
+            }
+            return true;
+        } catch (NoSuchAlgorithmException e) {
+            // "RSA" is guaranteed to be available
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            //Invalid key specification
+        } catch (SignatureException e) {
+            //Signature exception
+        }
+        return false;
+    }
+}
